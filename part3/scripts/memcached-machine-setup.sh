6329bfa7a3418746b1aeb385e54bd6e31d062336
Gianlo <gianlorenzo.occhipinti@gmail.com>
2026-04-21 11:04:32 +0200

    update script

--- part3/scripts/memcached-machine-setup.sh
+++ part3/scripts/memcached-machine-setup.sh
@@ -10,3 +10,11 @@ sudo apt-get build-dep memcached --yes
 git clone https://github.com/eth-easl/memcache-perf-dynamic.git
 cd memcache-perf-dynamic
 make
+
+
+######
+while true; do
+./mcperf -s 100.96.2.3 -a 10.0.16.8 -a 10.0.16.6 \
+  --noload -T 6 -C 4 -D 4 -Q 1000 -c 4 -t 10 --scan 30000:30500:5 \
+  | tee ~/mcperf_continuous.txt
+done
